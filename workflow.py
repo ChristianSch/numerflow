537f99bc738b74ae518595fdff4bedc4bd72db0b
Christian Schulze <christian.schulze@mni.thm.de>
2016-08-25 13:49:24 +0200

    completed workflow task parameter pass through

--- workflow.py
+++ workflow.py
@@ -5,6 +5,9 @@ from tasks.numerai_upload_predictions import UploadPredictions
 
 
 class Workflow(luigi.Task):
+    dataset_path = luigi.Parameter(
+        default='https://datasets.numer.ai/57b4899/numerai_datasets.zip')
+    output_path = luigi.Parameter(default='./data/')
     usermail = luigi.Parameter()
     userpass = luigi.Parameter()
 
@@ -16,5 +19,8 @@ class Workflow(luigi.Task):
         for d in data:
             target = d.output()
 
-            UploadPredictions(filepath=target.path, usermail=self.usermail,
-                              userpass=self.userpass)
+            UploadPredictions(filepath=target.path,
+                              usermail=self.usermail,
+                              userpass=self.userpass,
+                              dataset_path=self.dataset_path,
+                              output_path=self.output_path)
