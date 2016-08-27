7923cdda0ffbc98cc4a62ee478174d99ecb63177
Christian Schulze <christian.schulze@mni.thm.de>
2016-08-27 11:20:50 +0200

    fixed workflow upload task dependency; fixed parameter passing; dataset_path is now fetched via the api to guarantee the most current zip file

--- workflow.py
+++ workflow.py
@@ -3,24 +3,27 @@ import luigi
 from tasks.numerai_train_and_predict import TrainAndPredict
 from tasks.numerai_upload_predictions import UploadPredictions
 
+from controllers.ApiController import ApiController
+
 
 class Workflow(luigi.Task):
-    dataset_path = luigi.Parameter(
-        default='https://datasets.numer.ai/57b4899/numerai_datasets.zip')
     output_path = luigi.Parameter(default='./data/')
     usermail = luigi.Parameter()
     userpass = luigi.Parameter()
 
     def run(self):
+        apc = ApiController()
+        dataset_path = apc.fetch_current_dataset_uri()
         data = [
-            TrainAndPredict()
+            TrainAndPredict(dataset_path=dataset_path,
+                            output_path=self.output_path)
         ]
 
         for d in data:
             target = d.output()
 
-            UploadPredictions(filepath=target.path,
-                              usermail=self.usermail,
-                              userpass=self.userpass,
-                              dataset_path=self.dataset_path,
-                              output_path=self.output_path)
+            yield UploadPredictions(filepath=target.path,
+                                    usermail=self.usermail,
+                                    userpass=self.userpass,
+                                    dataset_path=dataset_path,
+                                    output_path=self.output_path)
